2f244109aa09d5297de3510533b05d2494df418b
Sushant <sushantshetty09@gmail.com>
2025-08-27 08:24:12 +0530

    1stpandas.py modified

--- 1stpandas.py
+++ 1stpandas.py
@@ -1,8 +1,21 @@
 import pandas as pd
 data={"name":["sushant","sanket"],
       "age":[21,22],
-      "cgpa":[8.9,9]}
+      "cgpa":[8.9,4]}
 df=pd.DataFrame(data)
-print(df.name)
-print(df.age)
-print(df.name,df.age)
+# print(df.name)
+# print(df.age)
+# # print(df.name,df.age)
+# print(df.iloc[1])
+# print(df[0:2])
+# # print(df[:])
+# print(df[df['age']>20])
+# print(df[df['name']=='sanket'])
+
+# print(df.isnull())
+
+# print(df.dropna())
+
+# print(df.fillna(0))
+
+print(df.rename(columns={"name":"full name"},inplace=True))
